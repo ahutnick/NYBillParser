28935b4ddc8da0c5480fb115e50857b9c76cf1ce
Tasha Hutnick <ahutnick@sas.upenn.edu>
2023-06-09 14:28:16 -0400

    update existing files to react-router-dom v6

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Layout from "./layout/layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./layout/Layout";
 import './App.css';
 
 function App() {
   return (
-    <Switch>
-      <Route path ="/">
-        <Layout />
-      </Route>
-    </Switch>
+    <BrowserRouter>
+      <Routes>
+        <Route path ="/" element={<Layout />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- front-end/src/layout/Layout.js
+++ front-end/src/layout/Layout.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 function Layout() {
-    return (
-        null
-        /* Header
-           Routes */
-    );
+    return <h1>Hello</h1>
 }
 
 export default Layout;
