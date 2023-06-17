be1c083566c10aa9b4e12eab769cfb2ef85f6512
Tasha Hutnick <ahutnick@sas.upenn.edu>
2023-06-17 13:57:23 -0400

    add bills year test

--- back-end/test/us-01-list-ny-bills.test.js
+++ back-end/test/us-01-list-ny-bills.test.js
@@ -2,6 +2,8 @@ const request = require("supertest");
 
 const app = require("../src/app");
 
+// Move to Own file
+
 describe("US-01 List NY Bills", () => {
     describe("App", () => {
         describe("not found handler", () => {
@@ -15,4 +17,24 @@ describe("US-01 List NY Bills", () => {
             });
         });
     });
+
+    // Pulls bills from this year X
+    // Pulls bills that aren't signed
+
+    describe("GET /bills", () => {
+        test("returns only bills from this year", async () => {
+            const response = await request(app)
+                .get("/bills")
+                .set("Accept", "application/json");
+            
+            const year = new Date().getFullYear();
+
+            const { status, body: { data } } = response;
+
+            data.forEach(element => {
+                expect(element).toHaveProperty('year', year);
+            });
+            expect(status).toBe(200);
+        });
+    })
 }) 
