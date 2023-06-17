df93d79351cb816b57f0bb89fc18c8a7474653a7
Tasha Hutnick <ahutnick@sas.upenn.edu>
2023-06-17 13:25:13 -0400

    404 test passing

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,7 +1,13 @@
 const path = require("path");
 
+const errorHandler = require("./errors/errorHandler");
+const notFound = require("./errors/notFound");
+
 const express = require("express");
 
 const app = express();
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
