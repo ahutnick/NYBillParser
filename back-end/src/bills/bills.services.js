7d9f953f2ede8c37152d9936ab5edd75fef3f2eb
Tasha Hutnick <ahutnick@sas.upenn.edu>
2023-06-17 18:53:25 -0400

    add info view

--- back-end/src/bills/bills.services.js
+++ back-end/src/bills/bills.services.js
@@ -2,7 +2,7 @@ import got from "got";
 
 async function list() {
     const year = new Date().getFullYear();
-    const url = `https://legislation.nysenate.gov/api/3/bills/${year}/search?term=adopted:false&key=${process.env.API_KEY}`
+    const url = `https://legislation.nysenate.gov/api/3/bills/${year}/search?term=adopted:false&view=info&key=${process.env.API_KEY}`
     const options = {timeout: { request: 15000 } };
     const response = await got(url, options).json();
     return response.result.items.map(item => item.result);
