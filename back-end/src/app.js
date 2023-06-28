f68f1bce759d72345d8976d158fab5cb74178fdb
Tasha Hutnick <ahutnick@sas.upenn.edu>
2023-06-27 20:03:43 -0400

    add cors

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,5 +1,7 @@
 const path = require("path")
 
+const cors = require("cors");
+
 const express = require("express");
 
 const dotenv = require("dotenv");
@@ -9,9 +11,16 @@ const notFound = require("./errors/notFound");
 
 const billsRouter = require("./bills/bills.router");
 
+const corsOptions = {
+    origin: "http://localhost:3000",
+    optionsSuccessStatus: 200
+}
+
 dotenv.config();
 
 const app = express();
+
+app.use(cors(corsOptions));
 
 app.use("/bills", billsRouter);
 
