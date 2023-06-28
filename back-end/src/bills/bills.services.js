1edad223a3bb279bd4789c9691e06405b59c2814
Tasha Hutnick <ahutnick@sas.upenn.edu>
2023-06-27 20:04:33 -0400

    update services to access data first

--- back-end/src/bills/bills.services.js
+++ back-end/src/bills/bills.services.js
@@ -4,7 +4,7 @@ async function list() {
     const year = new Date().getFullYear();
     const url = `https://legislation.nysenate.gov/api/3/bills/${year}/search?term=adopted:false&view=info&key=${process.env.API_KEY}`;
     const response = await axios.get(url, {timeout: 15000});
-    return response.result.items.map(item => item.result);
+    return response.data.result.items.map(item => item.result);
 }
 
 module.exports = {
