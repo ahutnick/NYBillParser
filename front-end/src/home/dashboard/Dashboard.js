8808f3f1f58f2982ab5c43813d76921979b288e2
Tasha Hutnick <ahutnick@sas.upenn.edu>
2023-06-30 16:20:10 -0400

    add ignore to loadbills and add accordion

--- front-end/src/home/dashboard/Dashboard.js
+++ front-end/src/home/dashboard/Dashboard.js
@@ -1,25 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { listBills } from "../../utils/api";
 import ErrorAlert from "../../utils/ErrorAlert";
+import BillAccordion from "./bills/BillAccordion";
 
 function Dashboard() {
     const [bills, setBills] = useState([]);
     const [billsErrors, setBillsErrors] = useState([]);
 
-    useEffect(loadBills, []);
+    useEffect(() => {
+        let ignore = false;
+        
+        async function loadBills() {
+            const abortController = new AbortController();
+            setBillsErrors(null);
+            try {
+            const bills = await listBills(abortController.signal)
+            if (!ignore) {
+                setBills(bills)
+            }
+        } catch (error) {
+            setBillsErrors(error);
+        }
+            return () => {
+                abortController.abort()
+                ignore = true;
+            };
+        }
 
-    function loadBills() {
-        const abortController = new AbortController();
-        setBillsErrors(null);
-        listBills(abortController.signal)
-            .then(setBills)
-            .catch(setBillsErrors);
-        console.log(bills);
-        return () => abortController.abort();
-    }
+        loadBills();
+    }, []);
+
+    
 
     return (<div>
             <h1>Hello</h1>
+            <BillAccordion bills={bills}/>
             <ErrorAlert error={billsErrors}/>
         </div>);
 
