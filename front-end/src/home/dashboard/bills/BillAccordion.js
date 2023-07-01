e975eb3e728fea71491cba998718c1cba26ff4cc
Tasha Hutnick <ahutnick@sas.upenn.edu>
2023-07-01 14:11:30 -0400

    Add no bills message

--- front-end/src/home/dashboard/bills/BillAccordion.js
+++ front-end/src/home/dashboard/bills/BillAccordion.js
@@ -3,8 +3,8 @@ import Bill from "./Bill";
 
 function BillAccordion({bills}) {
     return (
-        <div className="accordion mb-5" id="billAccordion">
-            {bills.length ? bills.map((bill, index) => <Bill bill={bill} index={index} key={bill.printNo} />) : null }
+        <div className="accordion" id="billAccordion">
+            {bills.length ? bills.map((bill, index) => <Bill bill={bill} index={index} key={bill.printNo} />) : "No bills currently" }
         </div>
     )
 }
